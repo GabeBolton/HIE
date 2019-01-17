fb4ce194aa4a9f3e65b78a3c535ffe60ce04bedb
GabeBolton <33410751+GabeBolton@users.noreply.github.com>
2019-01-17 17:08:57 +1100

    Update pandasFileConcatenation.py

--- pandasFileConcatenation.py
+++ pandasFileConcatenation.py
@@ -4,5 +4,5 @@ folderPath=''#subpath from working directory where files are
 file = glob.glob(folderPath+'\\*.dat')#matches all .dat files
 fileData=[]
 for file in files:
-    fileData.append(pd.read_csv(file,index_col=0,skiprows=[0,2,3]))
+    fileData.append(pd.read_csv(file,index_col=0))# use skiprows=[0,1,2,5] etc. to skip rows
 concat=pd.concat(fileData[:], ignore_index=False,sort=False)
