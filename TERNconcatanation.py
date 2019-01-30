b709d5e503ccaa8105f612d148c5a8bb26e99e44
GabeBolton <33410751+GabeBolton@users.noreply.github.com>
2019-01-30 14:09:05 +1100

    Noise Trimming

--- TERNconcatanation.py
+++ TERNconcatanation.py
@@ -69,3 +69,47 @@ plt.title('x: PA_uS_Avg(3), y: VW_Avg(3)')
 plt.subplot(326)
 plt.scatter(concat.loc[:,'PA_uS_Avg(3)'],concat.loc[:,'VWC(3)'],marker='.',s=1)
 plt.title('x: PA_uS_Avg(3), y: VWC(3)')
+
+#noise trimming
+trimmed=concat[concat.loc[:,'PA_uS_Avg(1)']>15]
+trimmed=trimmed[(trimmed.loc[:,'PA_uS_Avg(2)']>15)]
+trimmed=trimmed[(trimmed.loc[:,'PA_uS_Avg(3)']>15)]
+
+plt.subplots(figsize=[10,10])
+plt.subplot(321)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(1)'],trimmed.loc[:,'VW_Avg(1)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(1), y: VW_Avg(1)')
+plt.subplot(322)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(1)'],trimmed.loc[:,'VWC(1)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(1), y: VWC(1)')
+
+plt.subplot(323)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(2)'],trimmed.loc[:,'VW_Avg(2)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(2), y: VW_Avg(2)')
+plt.subplot(324)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(2)'],trimmed.loc[:,'VWC(2)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(2), y: VWC(2)')
+
+plt.subplot(325)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(3)'],trimmed.loc[:,'VW_Avg(3)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(3), y: VW_Avg(3)')
+plt.subplot(326)
+plt.scatter(trimmed.loc[:,'PA_uS_Avg(3)'],trimmed.loc[:,'VWC(3)'],marker='.',s=1)
+plt.title('trimmed   x: PA_uS_Avg(3), y: VWC(3)')
+
+plt.subplots(figsize=[30,10])
+plt.subplot(3,1,1)
+plt.scatter(trimmed.index,trimmed.loc[:,'PA_uS_Avg(1)'],marker='.',s=1)
+plt.title('trimmed PA_uS_Avg(1)')
+plt.ylim(-1,40)
+plt.grid(True)
+plt.subplot(3,1,2)
+plt.scatter(trimmed.index,trimmed.loc[:,'PA_uS_Avg(2)'],marker='.',s=1)
+plt.title('trimmed PA_uS_Avg(2)')
+plt.ylim(-1,40)
+plt.grid(True)
+plt.subplot(3,1,3)
+plt.scatter(trimmed.index,trimmed.loc[:,'PA_uS_Avg(3)'],marker='.',s=1)
+plt.title('trimmed PA_uS_Avg(3)')
+plt.ylim(-1,40)
+plt.grid(True)
